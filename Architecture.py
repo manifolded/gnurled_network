56e714488e3ef625641b27bd7c40fa28ba336894
Keith Matthews <keith@keithdmatthews.com>
2022-08-15 14:28:22 -0700

    feat: added args for setting weights on consruction

--- Architecture.py
+++ Architecture.py
@@ -1,19 +1,32 @@
 import numpy as np
 
 class Node():
-
+    def __init__(self, init_input_weights_1D: np.array, input_layer):
+        # init_input_weights_1D must be 1D for a simple node
+        assert(init_input_weights_1D.shape == (input_layer.size(), 1) or \
+                init_input_weights_1D.shape == (input_layer.size(), ))
+            
     def __init__(self, input_layer):
         self.input_layer = input_layer
-        self.input_weights = [1.0] * self.input_layer.size()
+        self.input_weights = init_input_weights_1D
 
     def output(self) -> np.float32:
         return sum(map(lambda x,y:x*y, self.input_weights, self.input_layer.outputs()))
 
 
 class Layer():
+    def __init__(self, size: int, init_input_weights_2D: np.array, input_layer):
+        assert(len(init_input_weights_2D.shape) == 2)
+    
+        # If the immediately upstream layer is a Value_layer, require that it 
+        # have same size as this layer.
+        assert init_input_weights_2D.shape == (input_layer.size(), size),\
+        'init_input_weights_2D must be a matrix of dimension ({}, {}) not {}!'\
+        .format(input_layer.size(), size, init_input_weights_2D.shape)
 
-    def __init__(self, size: int, input_layer):
-        self.nodes = [Node(input_layer)] * size
+        self.nodes = []
+        for n in range(size):
+            self.nodes.append(Node(init_input_weights_2D[:,n], input_layer))
 
     def size(self):
         return len(self.nodes)
@@ -26,20 +39,25 @@ class Layer():
 class ValueLayer():
 
     def __init__(self, global_input_values: list[np.float32]):
+class Value_layer():
+    def __init__(self, global_input_values: np.array):
+        assert(np.linalg.matrix_rank(global_input_values) == 1)
         self.global_input_values = global_input_values
 
     def size(self):
-        return len(self.global_input_values)
+        return self.global_input_values.shape[0]
 
     def outputs(self) -> list[np.float32]:
         return self.global_input_values
 
-    def reset_global_input_values(self, global_input_values: list[np.float32]):
+    def adjust_global_input_values(self, global_input_values: np.array):
         self.global_input_values = global_input_values
 
 input_values = [1.0, -0.5]
 layer0 = ValueLayer(input_values)
 layer1 = Layer(3, layer0)
+input_values = np.array([1.0, -0.5, 2.0])
+layer0 = Value_layer(input_values)
 layers = [layer0, layer1]
 
 print([val for val in layers[-1].outputs()])
