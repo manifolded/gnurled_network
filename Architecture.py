bdb68b3ef83c090b9d2a92178ede3508503285fb
Keith Matthews <keith@keithdmatthews.com>
2022-08-15 14:31:08 -0700

    feat: Gradually adding asserts

--- Architecture.py
+++ Architecture.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class Node():
     def __init__(self, init_input_weights_1D: np.array, input_layer):
+        assert(input_layer.size() > 0)
         # init_input_weights_1D must be 1D for a simple node
         assert(init_input_weights_1D.shape == (input_layer.size(), 1) or \
                 init_input_weights_1D.shape == (input_layer.size(), ))
@@ -11,15 +12,20 @@ class Node():
         self.input_weights = init_input_weights_1D
 
     def output(self) -> np.float32:
-        return sum(map(lambda x,y:x*y, self.input_weights, self.input_layer.outputs()))
+        return sum(map(lambda x,y: x*y, self.input_weights, self.input_layer.outputs()))
 
 
 class Layer():
     def __init__(self, size: int, init_input_weights_2D: np.array, input_layer):
+        assert(size > 0)
         assert(len(init_input_weights_2D.shape) == 2)
     
         # If the immediately upstream layer is a Value_layer, require that it 
         # have same size as this layer.
+        if isinstance(input_layer, Value_layer):
+            assert input_layer.size() == size,\
+            'upstream is Value_layer -> sizes must match'
+        
         assert init_input_weights_2D.shape == (input_layer.size(), size),\
         'init_input_weights_2D must be a matrix of dimension ({}, {}) not {}!'\
         .format(input_layer.size(), size, init_input_weights_2D.shape)
