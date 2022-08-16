a82d2f87b40ae40895eb00a9d21b0b8bb9914fe1
Keith Matthews <keith@keithdmatthews.com>
2022-08-16 10:55:53 -0700

    chore: Reposition docstrings

--- Architecture.py
+++ Architecture.py
@@ -1,20 +1,20 @@
 import numpy as np
 from math import exp
 
-"""
-Nodes are the vertices in the network. They provide a minimal unit of 
-computation. Nodes are combined to make layers, see class Layers for 
-more detail. 
-
-The outputs of the nodes of the layer immediately upstream provide
-each node's inputs. These inputs are weighted before being combined into a 
-single output scalar. 
-
-By design nodes own the weights for their connections with the nodes of the 
-directly upstream layer. They do not own the weights for their downstream 
-connections which are owned instead for the nodes of the next layer.
-"""
 class Node():
+    """
+    Nodes are the vertices in the network. They provide a minimal unit of 
+    computation. Nodes are combined to make layers, see class Layers for 
+    more detail. 
+
+    The outputs of the nodes of the layer immediately upstream provide
+    each node's inputs. These inputs are weighted before being combined into a 
+    single output scalar. 
+
+    By design nodes own the weights for their connections with the nodes of the 
+    directly upstream layer. They do not own the weights for their downstream 
+    connections which are owned instead for the nodes of the next layer.
+    """
     def __init__(self, init_input_weights_1D: np.array, init_bias: np.float32, input_layer):
         assert(input_layer.size() > 0)
         # init_input_weights_1D must be 1D for a simple node
@@ -29,21 +29,21 @@ class Node():
         return sigmoid(sum(map(lambda x,y: x*y, self.input_weights, self.input_layer.outputs())) + self.bias)
 
 
-"""
-Layers are ranks of nodes of any length. These nodes all receive their inputs
-from the nodes of the previous (upstream) layer, and output their computations
-to the nodes of the next (downstream) layer.
-
-The number of inputs to any node is always the size of the previous layer 
-since every one of its nodes' output is one of this node's inputs.
-
-Layers breakup the provided init_input_weights_2D and construct each node with
-the appropriate column of the matrix.
-
-A Layer must check if its previous layer is a InjectionLayer because this 
-situation dictates that the init_input_weights_2D be the identity matrix.
-"""
 class Layer():
+    """
+    Layers are ranks of nodes of any length. These nodes all receive their inputs
+    from the nodes of the previous (upstream) layer, and output their computations
+    to the nodes of the next (downstream) layer.
+
+    The number of inputs to any node is always the size of the previous layer 
+    since every one of its nodes' output is one of this node's inputs.
+
+    Layers breakup the provided init_input_weights_2D and construct each node with
+    the appropriate column of the matrix.
+
+    A Layer must check if its previous layer is a InjectionLayer because this 
+    situation dictates that the init_input_weights_2D be the identity matrix.
+    """
     def __init__(self, size: int, init_input_weights_2D: np.array, init_biases_1D: np.array, input_layer):
         assert(size > 0)
         assert(len(init_input_weights_2D.shape) == 2)
@@ -72,14 +72,14 @@ class Layer():
         return np.array([node.output() for node in self.nodes])
 
 
-""" 
-A value layer is a layer that produces pre-set values. It is intended to 
-serve in the role of a fake extra input_layer that provides values to the 
-actual input layer. While it pretends to be a 'Layer' it has no nodes 
-and only spits out exactly what you last specified with the
-adjust_global_input_values method.
-"""
 class InjectionLayer():
+    """ 
+    An injection layer is a layer that produces pre-set values. It is intended 
+    to serve in the role of a fake extra input_layer that provides values to 
+    the actual input layer. While it pretends to be a 'Layer' it has no nodes 
+    and only spits out exactly what you last specified with the 
+    adjust_global_input_values method.
+    """
     def __init__(self, global_input_values: np.array):
         assert(np.linalg.matrix_rank(global_input_values) == 1)
         self.global_input_values = global_input_values
@@ -99,13 +99,13 @@ class InjectionLayer():
         return np.identity(self.size())
 
 
-"""
-Implementation of the classic node activation function called the sigmoid 
-function, $\frac{1}{1 + e^{-x}}$. Despite the fact that I will inevitably be 
-coding up the derivative before long, I don't think I'm going to make this a
-class. I don't want the overhead of a constructor.
-"""
 def sigmoid(input: np.float32) -> np.float32:
+    """
+    Implementation of the classic node activation function called the sigmoid 
+    function, $\\frac{1}{1 + e^{-x}}$. Despite the fact that I will inevitably 
+    be coding up the derivative before long, I don't think I'm going to make 
+    this a class. I don't want the overhead of a constructor.
+    """
     return 1./(1. + exp(- input))
 
 
