0be2189715ced6000673a6589fa4513ab906f073
Keith Matthews <keith@keithdmatthews.com>
2022-08-25 00:25:25 -0700

    feat: Adds unit test for a back-prop deriv func

--- unit_tests.py
+++ unit_tests.py
@@ -10,7 +10,7 @@ sys.path.append(
     os.path.dirname(os.path.realpath(__file__))
 )
 from network import Network
-from utils import BinaryCrossEntropy, ArrayUtils, CategoricalCrossEntropy
+from utils import BinaryCrossEntropy, ArrayUtils, CategoricalCrossEntropy, Activation
 
 @pytest.fixture
 def single_feature_geometric_instances(num_examples: int):
@@ -22,7 +22,11 @@ def test_Given_oneOneNetwork_When_unitInput_Then_layer_0_coalesced_output():
     network = Network((1,1), ArrayUtils.all_ones_array, BinaryCrossEntropy)
     assert_array_equal(network.layers[0]._coalesced_inputs(unit_array), unit_array) 
 
-# Write type sensitive unit test that complains about np.float64
+# --- Write type sensitive unit test that complains about np.float64 ---
+# def test_Given_oneOneNetwork_When_unitInput_Then_output_float32():
+#     unit_array = ArrayUtils.all_ones_array((1, 1))
+#     network = Network((1,1), ArrayUtils.all_ones_array, BinaryCrossEntropy)
+#     assert network.outputs(unit_array).dtype == np.dtype(np.float32)
 
 def test_Given_oneOneNetwork_When_unitInput_Then_layer_0_sigmoid_output():
     num_examples = 1
@@ -54,7 +58,7 @@ def test_Given_oneOneNetwork_When_multInputs_Then_binaryCrossEntropy_cost_agrees
     assert_almost_equal(network.cost(labels, network.outputs(input_array)), 
                         expected_cost, 6) 
 
-def test_Given_fakePredictions_When_compareBinAndCatCosts_Then_costsAgree():
+def test_Given_poorPredictions_When_compareBinAndCatCosts_Then_costsAgree():
     eps1 = 0.38
     labels = np.full((2,1), [[1. - eps1], [0. + eps1]], dtype=np.float32)
     eps2 = 0.053
@@ -63,14 +67,43 @@ def test_Given_fakePredictions_When_compareBinAndCatCosts_Then_costsAgree():
     bin_cost = BinaryCrossEntropy.cost(labels[0], predictions[0])
     assert_almost_equal(cat_cost, bin_cost, 6)
 
-def test_Given_fakePredictions_When_compareBinAndCatDerivs_Then_derivsAgree():
+def test_Given_poorPredictions_When_compareBinAndCatDerivs_Then_derivsAgree():
     eps1 = 0.38
     labels = np.full((2,1), [[1. - eps1], [0. + eps1]], dtype=np.float32)
     eps2 = 0.053
     predictions = np.full((2,1), [[1.0 - eps2], [0.0 + eps2]], dtype=np.float32)
     cat_deriv = CategoricalCrossEntropy.cost_deriv(labels, predictions) 
     bin_deriv = BinaryCrossEntropy.cost_deriv(labels[0], predictions[0])
-    print(cat_deriv.shape, bin_deriv.shape)
     assert_almost_equal(np.sum(cat_deriv), bin_deriv, 5)
 
 # Next up? Back-propagation...
+@pytest.fixture
+def balanced_pair_activations():
+    eps = 0.21
+    return np.full((2,), [[1.0 - eps], [0.0 + eps]], dtype=np.float32)
+
+def test_Given_1layerNetwork_When_deriv_a_wrt_z_Then_agrees():
+    inputs = 1. - np.arange(0.15, 0.75, 0.1, dtype=np.float32).reshape((2,3))
+    network = Network((2), (lambda x: ArrayUtils.identity_arrays_and_uniform_vectors(x, 0.)), CategoricalCrossEntropy)
+    deriv_a_wrt_z = network._deriv_a_wrt_z(0, inputs)
+    verified_deriv_a_wrt_z = np.array([[0.209773,0.217895,0.225348],[0.232008,0.237759,0.242497]])
+    assert_almost_equal(deriv_a_wrt_z, verified_deriv_a_wrt_z, 6)
+
+
+# def test_Given_1_1_network_When_backProp_Then_deltasZero():
+#     eps1 = 0.87675
+#     labels = np.full((1,), [1. - eps1], dtype=np.float32)
+#     eps2 = 0.053
+#     inputs = np.full((1,), [1.0 - eps2], dtype=np.float32)
+#     network = Network((1,1), (lambda x: ArrayUtils.identity_arrays_and_uniform_vectors(x, 0.)), BinaryCrossEntropy)
+#     # Compute delta_weights_and_biases, the automatic approach
+#     delta_weights_and_biases = network.compute_delta_weights_and_biases(labels, inputs, 1.0)
+#     # Compute deltas semi-automatically
+#     predictions = network.outputs(inputs)
+#     deriv_cost = BinaryCrossEntropy.cost_deriv(labels, predictions)
+#     layer0_outputs = Activation.sigmoid(inputs)
+#     deriv_af_wrt_zf = Activation.deriv_sig(layer0_outputs)
+#     # Note the down-indexing of the deriv_cost term. This auto-dim-extending 
+#     # code in the Activation class is really ugly and needs to be fixed!!!
+#     delta_weights_f = np.einsum('n,n,p -> pn', deriv_cost[:,0], deriv_af_wrt_zf, layer0_outputs)
+#     assert_array_equal(delta_weights_and_biases[1][0], delta_weights_f)
